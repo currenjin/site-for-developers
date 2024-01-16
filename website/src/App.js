5e7c330e42b8ab8a98b4e491c427ec7c911ea663
hyeonho2010 <hyeonhokang10@kakao.com>
2024-01-17 08:15:42 +0900

    Fix markdown display error

--- website/src/App.js
+++ website/src/App.js
@@ -4,16 +4,15 @@ import { marked } from 'marked';
 import axios from 'axios';
 
 const App = () => {
-  const [users, setUsers] = useState([]);
+  const [mdData, setmdData] = useState([]);
 
   axios.get('https://cdn.jsdelivr.net/gh/currenjin/site-for-developers/README.md')
     .then(response => {
       console.log(marked.parse(response.data));
-      setUsers(marked.parse(response.data));
+      setmdData(marked.parse(response.data));
     });
   return (
-    <div className="ml-4 mr-4 mb-4 p-4 rounded-lg bg-slate-200">
-      {users}
+    <div className="ml-4 mr-4 mb-4 p-4 rounded-lg bg-slate-200 pretendard" dangerouslySetInnerHTML={{ __html: mdData }}>
     </div>
   );
 }
