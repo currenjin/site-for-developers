b8038f61aeb19890884ed04b4e8e7256676297d9
hyeonho2010 <hyeonhokang10@kakao.com>
2024-01-20 18:56:18 +0900

    Fix gfm rendering error. (https://github.com/currenjin/site-for-developers/issues/9)

--- website/src/App.js
+++ website/src/App.js
@@ -1,36 +1,14 @@
 import "./css/App.css";
 import { React } from "react";
-import { IoClose, IoMenu } from "react-icons/io5";
+import { IoMenu } from "react-icons/io5";
 import { Route, Routes, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contribute from "./pages/Contribute";
 
 const App = () => {
-  function closeBanner() {
-    var banner = document.getElementById("banner");
-    banner.style.display = "none";
-  }
   return (
     <>
-      <div
-        className="banner flex items-center justify-between w-full bg-gray-200 p-4 suit"
-        id="banner"
-      >
-        <div className="justify-start">
-          현재 목차를 클릭하였을 때 정상적으로 작동하지 않는 오류가 있어요.
-          자세한 내용을 보려면{" "}
-          <a href="https://github.com/currenjin/site-for-developers/issues/9">
-            여기
-          </a>
-          를 클릭하세요.
-        </div>
-        <div className="justify-end">
-          <button className="bg-gray-300 p-2 rounded-lg" onClick={closeBanner}>
-            <IoClose size={22} />
-          </button>
-        </div>
-      </div>
       <div className="navbar bg-base-100">
         <div className="flex-none"></div>
         <div className="flex-1">
