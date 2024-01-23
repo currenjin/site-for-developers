ff08ba51a48a235cadd5d7f720726acd606a83cc
Hyeonho Kang <hyeonhokang10@kakao.com>
2024-01-23 11:33:37 +0900

    Fix conflicts

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import "./components/css/App.css";
+import { React } from "react";
+import { IoMenu } from "react-icons/io5";
+import { Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contribute from "./pages/Contribute";
+
+const App = () => {
+  return (
+    <>
+      <div className="navbar bg-base-100">
+        <div className="flex-none"></div>
+        <div className="flex-1">
+          <Link to="/">
+            <div className="btn btn-ghost text-xl suit">
+              💎 Site for developers
+            </div>
+          </Link>
+        </div>
+        <div className="flex-none">
+          <div className="dropdown dropdown-hover dropdown-bottom dropdown-end">
+            <div tabIndex={0} role="button" className="p-2">
+              <IoMenu size={40} />
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content z-[1] menu p-1 shadow bg-base-100 rounded-box w-52 suit text-base"
+            >
+              <li>
+                <Link to="/">홈</Link>
+              </li>
+              <li>
+                <Link to="/about">소개</Link>
+              </li>
+              <li>
+                <Link to="/contribute">기여</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contribute" element={<Contribute />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,32 @@
+import "../components/css/pages.about.css";
+import axios from "axios";
+import { useState, React } from "react";
+
+const About = () => {
+  const [repositoryData, setrepositoryData] = useState([]);
+
+  axios
+    .get("https://api.github.com/repos/currenjin/site-for-developers")
+    .then((response) => {
+      console.log(response.data);
+      setrepositoryData(response.data);
+    });
+
+  return (
+    <div className="repositorydata ml-4 mr-4 mb-4 p-4 rounded-lg bg-gray-100 pretendard text-wrap">
+      <span>이름: {repositoryData.full_name}</span>
+      <br />
+      <span>소개: {repositoryData.description}</span>
+      <br />
+      <span>개설일: {repositoryData.created_at}</span>
+      <br />
+      <span>최근 업데이트: {repositoryData.updated_at}</span>
+      <br />
+      <span>웹사이트 주소: {repositoryData.homepage}</span>
+      <br />
+      <span>스타 수: {repositoryData.stargazers_count}개</span>
+    </div>
+  );
+};
+
+export default About;
