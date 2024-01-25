78520fcf70edd76df5e6aabbc901b7a0efa61925
currenjin <hyun0524e@naver.com>
2024-01-25 22:32:52 +0900

    Feat: Convert yml to md

---
+++ .github/workflows/convert-yml-to-md.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+function generateMarkdown(categories, sites) {
+  let markdown = '## 문서\n';
+  categories.forEach(category => {
+    markdown += `### ${category.text}\n`;
+    const categorySites = sites.filter(site => site.categories.includes(category.category));
+    categorySites.forEach(site => {
+      markdown += `- [${site.name}](${site.link}) - ${site.description}\n`;
+    });
+  });
+  return markdown;
+}
+
+const categories = require('./categories.yml');
+const sites = require('./sites.yml');
+
+const readmeContent = generateMarkdown(categories, sites);
+fs.writeFileSync('README.md', readmeContent);
