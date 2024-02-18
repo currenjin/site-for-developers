3ddfce0d888073c4310697ad2224bd0e94faef51
Hyeonho Kang <hyeonhokang10@kakao.com>
2024-02-18 16:47:00 +0900

    Add sitemap (#35)

---
+++ src/sitemapRoutes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Route } from 'react-router';
+
+export default (
+    <Route>
+        <Route path="/" />
+        <Route path="/about" />
+        <Route path="/contribute" />
+    </Route>
+);
