55176ed970c8af98f032e7392430c5d6c2cdb0db
Hyeonho Kang <hyeonhokang10@kakao.com>
2024-03-09 23:16:02 +0900

    Add analytics. (#46)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,37 @@
 import '../components/css/pages.home.css';
 import axios from "axios";
-import { marked } from "marked";
-import { gfmHeadingId } from "marked-gfm-heading-id";
-import { useState, React } from "react";
+import {marked} from "marked";
+import {gfmHeadingId} from "marked-gfm-heading-id";
+import {useState, React} from "react";
 
 const Home = () => {
-  const [mdData, setmdData] = useState([]);
+    const [mdData, setmdData] = useState([]);
 
-  const options = {
-    prefix: "",
-  };
+    const options = {
+        prefix: "",
+    };
 
-  marked.use({
-    gfm: true,
-  },
-  gfmHeadingId(options),);
+    marked.use({
+            gfm: true,
+        },
+        gfmHeadingId(options),);
 
-  axios
-    .get("https://raw.githubusercontent.com/currenjin/site-for-developers/main/README.md")
-    .then((response) => {
-      console.log(response.data);
-      setmdData(marked.parse(response.data));
-    });
+    axios
+        .get("https://raw.githubusercontent.com/currenjin/site-for-developers/main/README.md")
+        .then((response) => {
+            console.log(response.data);
+            setmdData(marked.parse(response.data));
+        });
 
     console.log(mdData);
-  return (
-    <div>
-      <div
-        className="suit mddata"
-        dangerouslySetInnerHTML={{ __html: mdData }}
-      ></div>
-    </div>
-  );
+    return (
+        <div>
+            <div
+                className="suit mddata"
+                dangerouslySetInnerHTML={{__html: mdData}}
+            ></div>
+        </div>
+    );
 };
 
 export default Home;
