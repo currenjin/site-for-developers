5de9c73b2444e3ee782aea5b5d53099289db640c
kenu <kenu@okjsp.pe.kr>
2024-06-14 02:39:22 +0900

    rm node_modules

--- src/App.js
+++ src/App.js
@@ -1,50 +1,71 @@
 import "./components/css/App.css";
-import {FaDiscord} from "react-icons/fa6";
-import {React} from "react"
-import {Link, Route, Routes} from "react-router-dom";
+import { FaDiscord } from "react-icons/fa6";
+import { React } from "react";
+import { Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contribute from "./pages/Contribute";
 
 const App = () => {
-    return (
-        <div>
-            <div className="suit navbar bg-base-100">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                                 stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                      d="M4 6h16M4 12h16M4 18h7"/>
-                            </svg>
-                        </div>
-                        <ul tabIndex={0}
-                            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><Link to="/">메인</Link></li>
-                            <li><Link to="/about">소개</Link></li>
-                            <li><Link to="/contribute">기여</Link></li>
-                        </ul>
-                    </div>
-                    <a className="btn btn-ghost text-xl">Site for developers</a>
-                </div>
-                <div className="navbar-end">
-                    <button className="btn btn-ghost btn-circle">
-                        <a href="https://discord.gg/kdVAeDGx" target="_blank">
-                            <FaDiscord size={30}/>
-                        </a>
-                    </button>
-                </div>
+  return (
+    <div>
+      <div className="suit navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
             </div>
-            <div className="m-4">
-                <Routes>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/about" element={<About/>}/>
-                    <Route path="/contribute" element={<Contribute/>}/>
-                </Routes>
-            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <Link to="/">메인</Link>
+              </li>
+              <li>
+                <Link to="/about">소개</Link>
+              </li>
+              <li>
+                <Link to="/contribute">기여</Link>
+              </li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl">Site for developers</a>
         </div>
-    );
+        <div className="navbar-end">
+          <button className="btn btn-ghost btn-circle">
+            <a href="https://discord.gg/kdVAeDGx" target="_blank">
+              <FaDiscord size={30} />
+            </a>
+          </button>
+        </div>
+      </div>
+      <div className="m-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contribute" element={<Contribute />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,38 +1,38 @@
 import "../components/css/pages.about.css";
 import axios from "axios";
-import {useState, React} from "react";
+import { useState, React } from "react";
 
 const About = () => {
-    const [repositoryData, setrepositoryData] = useState([]);
+  const [repositoryData, setrepositoryData] = useState([]);
 
-    axios
-        .get("https://api.github.com/repos/currenjin/site-for-developers")
-        .then((response) => {
-            console.log(response.data);
-            setrepositoryData(response.data);
-        });
+  axios
+    .get("https://api.github.com/repos/currenjin/site-for-developers")
+    .then((response) => {
+      console.log(response.data);
+      setrepositoryData(response.data);
+    });
 
-    return (
-        <div className="suit repositorydata">
-            <span>이름: {repositoryData.full_name}</span>
-            <br/>
-            <span>소개: {repositoryData.description}</span>
-            <br/>
-            <span>개설일: {repositoryData.created_at}</span>
-            <br/>
-            <span>최근 업데이트: {repositoryData.updated_at}</span>
-            <br/>
-            <span>레포지토리 상태: {repositoryData.visibility}</span>
-            <br/>
-            <span>웹사이트 주소: {repositoryData.homepage}</span>
-            <br/>
-            <span>스타 수: {repositoryData.stargazers_count}개</span>
-            <br/>
-            <span>구독자 수: {repositoryData.subscribers_count}명</span>
-            <br/>
-            <span>포크 횟수: {repositoryData.forks}번</span>
-        </div>
-    );
+  return (
+    <div className="suit repositorydata">
+      <span>이름: {repositoryData.full_name}</span>
+      <br />
+      <span>소개: {repositoryData.description}</span>
+      <br />
+      <span>개설일: {repositoryData.created_at}</span>
+      <br />
+      <span>최근 업데이트: {repositoryData.updated_at}</span>
+      <br />
+      <span>레포지토리 상태: {repositoryData.visibility}</span>
+      <br />
+      <span>웹사이트 주소: {repositoryData.homepage}</span>
+      <br />
+      <span>스타 수: {repositoryData.stargazers_count}개</span>
+      <br />
+      <span>구독자 수: {repositoryData.subscribers_count}명</span>
+      <br />
+      <span>포크 횟수: {repositoryData.forks}번</span>
+    </div>
+  );
 };
 
 export default About;
