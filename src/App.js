905e5d472ee7bbfe8e4841b6da6a84aad0a52b39
hyeonho Kang <hyeonhokang10@kakao.com>
2024-08-09 11:07:45 +0900

    Fix error

--- src/App.js
+++ src/App.js
@@ -7,55 +7,23 @@ import About from "./pages/About";
 import Contribute from "./pages/Contribute";
 
 const App = () => {
-    return (
-        <div>
-            <div className="suit navbar bg-base-100">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                                 stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                      d="M4 6h16M4 12h16M4 18h7"/>
-                            </svg>
-                        </div>
-                        <ul tabIndex={0}
-                            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><Link to="/">메인</Link></li>
-                            <li><Link to="/about">소개</Link></li>
-                            <li><Link to="/contribute">기여</Link></li>
-                        </ul>
-                    </div>
-                    <a className="btn btn-ghost text-xl">Site for developers</a>
-                </div>
-                <div className="navbar-end">
-                    <button className="btn btn-ghost btn-circle">
-                        <a href="https://discord.gg/kdVAeDGx" target="_blank">
-                            <FaDiscord size={30}/>
-                        </a>
-                    </button>
-                </div>
+  return (
+    <div>
+      <div className="suit navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h7" />
+              </svg>
             </div>
-            <div className="ml-2 mr-2">
-                <Routes>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/about" element={<About/>}/>
-                    <Route path="/contribute" element={<Contribute/>}/>
-                </Routes>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <Link to="/">메인</Link>
-              </li>
-              <li>
-                <Link to="/about">소개</Link>
-              </li>
-              <li>
-                <Link to="/contribute">기여</Link>
-              </li>
+            <ul tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+              <li><Link to="/">메인</Link></li>
+              <li><Link to="/about">소개</Link></li>
+              <li><Link to="/contribute">기여</Link></li>
             </ul>
           </div>
           <a className="btn btn-ghost text-xl">Site for developers</a>
@@ -68,7 +36,7 @@ const App = () => {
           </button>
         </div>
       </div>
-      <div className="m-4">
+      <div className="ml-2 mr-2">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
