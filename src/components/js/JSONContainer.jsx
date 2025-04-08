14f93c91cc9a8e75398b72ffbd83740cbd9c0148
Hyeonho Kang <hyeonhokang10@kakao.com>
2025-04-08 22:30:39 +0900

    Add favicon data (#102)

--- src/components/js/JSONContainer.jsx
+++ src/components/js/JSONContainer.jsx
@@ -1,24 +1,54 @@
-const JSONContainer = ({ category, title, data }) => {
-   return (
-     <section id={category}>
-       <h2 className="text-3xl font-bold md:py-3 py-2 px-1 text-indigo-600">{title}</h2>
-       <ul className="grid md:grid-cols-2 grid-cols-1 gap-3">
-         {data.map((item, index) => (
-           <li key={index} className="p-5 border border-indigo-600 rounded-2xl">
-             <a href={item.link} target="_blank" rel="noreferrer" className="text-xl font-semibold text-indigo-600 hover:underline">
-               {item.title}
-             </a>
-             <p className="text-sm mt-1">{item.content}</p>
-             <div className="mt-2 flex flex-wrap gap-1">
-               {item.feature?.map((feature) => (
-                 <span key={feature} className="badge badge-soft mr-2">{feature}</span>
-               ))}
-             </div>
-           </li>
-         ))}
-       </ul>
-     </section>
-   );
- };
+import { useEffect, useState } from "react";
 
- export default JSONContainer;
+const JSONContainer = ({ category, title, data }) => {
+  const [favicons, setFavicons] = useState({});
+
+  useEffect(() => {
+    const fetchFavicons = async () => {
+      const newFavicons = {};
+
+      for (const item of data) {
+        if (item.link) {
+          const url = new URL(item.link);
+          const faviconUrl = `https://www.google.com/s2/favicons?sz=64&domain=${url.origin}`;
+          newFavicons[item.link] = faviconUrl;
+        }
+      }
+      setFavicons(newFavicons);
+    };
+
+    fetchFavicons();
+  }, [data]);
+
+  return (
+    <section id={category}>
+      <h2 className="text-3xl font-bold md:py-3 py-2 px-1 text-indigo-600">{title}</h2>
+      <ul className="grid md:grid-cols-2 grid-cols-1 gap-3">
+        {data.map((item, index) => (
+          <li key={index} className="p-5 border border-indigo-600 rounded-2xl">
+            {favicons[item.link]?.trim() && (
+              <img
+                src={favicons[item.link]}
+                alt={`${item.link} favicon`}
+                className="rounded-lg"
+                width="32"
+                height="32"
+              />
+            )}
+            <a href={item.link} target="_blank" rel="noreferrer" className="text-xl font-semibold text-indigo-600 hover:underline">
+              {item.title}
+            </a>
+            <p className="text-sm mt-1">{item.content}</p>
+            <div className="mt-2 flex flex-wrap gap-1">
+              {item.feature?.map((feature) => (
+                <span key={feature} className="badge badge-soft mr-2">{feature}</span>
+              ))}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default JSONContainer;
